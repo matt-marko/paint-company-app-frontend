7255e87c0ec8dd7cef6e27c519527fa6aec4a622
Matt Marko <mattmarko321@gmail.com>
2024-03-24 13:43:34 -0700

    Add docs to service

--- src/app/services/paint-stock.service.ts
+++ src/app/services/paint-stock.service.ts
@@ -12,12 +12,16 @@ export class PaintStockService {
 
   private http: HttpClient = inject(HttpClient);
 
+  /**
+   * Gets the list of paints from the server.
+   * @returns An observable that emits an array of Paint objects.
+   */
   getPaints(): Observable<Paint[]> {
     return this.http.get<Paint[]>(this.apiUrl);
   }
 
   /**
-   * Updates the list of paints on the server.
+   * Updates the list of paints on the server with the new statuses.
    * @param paints The array of Paint objects to update.
    * @returns An observable that emits the updated array of Paint objects.
    */
